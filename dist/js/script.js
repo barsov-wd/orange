b8d565b94f29fdfec8c3b2ccd90cb518fe041130
Victor <barosv@yandex.ru>
2022-08-25 15:11:38 +0400

    на проверку

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,23 +1,23 @@
-function accord(headers, activeClass, paddingBottom) {
+window.addEventListener('DOMContentLoaded', () => {
+  // Menu
 
-    
-    const headers_ = document.querySelectorAll(headers);
+   const menu = document.querySelector('.header__menu'),
+   menuItem = document.querySelectorAll('.header__menu-item'),
+   humburger = document.querySelector('.humburger');
 
-    headers_.forEach(item => {
-        item.addEventListener('click', () => {
-            item.parentElement.classList.toggle(activeClass);
-            if (item.parentElement.classList.contains(activeClass)) {
-                item.nextElementSibling.style.cssText = `max-height: ${item.nextElementSibling.scrollHeight + paddingBottom}px; padding-bottom: ${paddingBottom}px;`;
-            } else {
-                item.nextElementSibling.style.cssText = `max-height: 0px; padding-bottom: 0px;`;
-            }
-        });
+   humburger.addEventListener('click', () => {
+      humburger.classList.toggle('humburger_active');
+      menu.classList.toggle('header__menu_active');
+   });
+
+   menuItem.forEach(item => {
+    item.addEventListener('click', () => {
+        humburger.classList.toggle('humburger_active');
+        menu.classList.toggle('header__menu_active');
     });
-}
+  });
 
-accord('.faq__item__header', 'faq__item--active', 20);
-
-const sumInput = document.querySelector('.calc__input--sum'),
+  const sumInput = document.querySelector('.calc__input--sum'),
         timeInput = document.querySelector('.calc__input--time'),
         payt = document.querySelector('.calc__pt'),
         rangeTimeInput = document.querySelector('.range__input--time'),
@@ -26,58 +26,58 @@ const sumInput = document.querySelector('.calc__input--sum'),
 
 
 
-// range + calc
+  // range + calc
 
-// маска
-function prettify(num) {
-var n = num.toString();
-return n.replace(/(\d{1,3}(?=(?:\d\d\d)+(?!\d)))/g, "$1" + ' ');
-}
+  // маска
+  function prettify(num) {
+  var n = num.toString();
+  return n.replace(/(\d{1,3}(?=(?:\d\d\d)+(?!\d)))/g, "$1" + ' ');
+  }
 
-function getPayment(sum, period, rate) {
-// *
-// * sum - сумма кредита
-// * period - срок в годах
-// * rate - годовая ставка в процентах
-// * payt - поле, куда будет вывобиться платеж
-let i,
-koef;
+  function getPayment(sum, period, rate) {
+  // *
+  // * sum - сумма кредита
+  // * period - срок в годах
+  // * rate - годовая ставка в процентах
+  // * payt - поле, куда будет вывобиться платеж
+  let i,
+  koef;
 
-// ставка в месяц
-i = (rate / 12) / 100;
+  // ставка в месяц
+  i = (rate / 12) / 100;
 
-// коэффициент аннуитета
-koef = (i * (Math.pow(1 + i, period * 12))) / (Math.pow(1 + i, period * 12) - 1);
+  // коэффициент аннуитета
+  koef = (i * (Math.pow(1 + i, period * 12))) / (Math.pow(1 + i, period * 12) - 1);
 
-// итог
-payt.textContent = (sum * koef).toFixed();
-}
+  // итог
+  payt.textContent = (sum * koef).toFixed();
+  }
 
 function range(input, progress, content) {
-$(input).each(function(i, elem) {
-val = $(this).val();
-min = $(this).attr('min');
-max = $(this).attr('max');
-step = $(this).attr('step');
-position = 100 / (max - step) * (val - step);
-$(this).parent().find(progress).width(position + "%");
-});
-$(input).on('input', function() {
-val = $(this).val();
-min = $(this).attr('min');
-max = $(this).attr('max');
-step = $(this).attr('step');
-position = 100 / (max - step) * (val - step);
-$(this).parent().find(progress).width(position + "%");
-content.value = prettify(val);
-});
+  $(input).each(function(i, elem) {
+  val = $(this).val();
+  min = $(this).attr('min');
+  max = $(this).attr('max');
+  step = $(this).attr('step');
+  position = 100 / (max - step) * (val - step);
+  $(this).parent().find(progress).width(position + "%");
+  });
+  $(input).on('input', function() {
+  val = $(this).val();
+  min = $(this).attr('min');
+  max = $(this).attr('max');
+  step = $(this).attr('step');
+  position = 100 / (max - step) * (val - step);
+  $(this).parent().find(progress).width(position + "%");
+  content.value = prettify(val);
+  });
 }
 
 function calc() {
-let sum = +sumInput.value.replace(/\D/g, ''),
-  period = +timeInput.value.replace(/\D/g, '');
+  let sum = +sumInput.value.replace(/\D/g, ''),
+    period = +timeInput.value.replace(/\D/g, '');
 
-getPayment(sum, period, 6);
+    getPayment(sum, period, 6);
 }
 
 
@@ -126,5 +126,89 @@ checkSymbols(timeInput, 'input', 10);
 checkSymbols(rangeSumInput, 'change');
 checkSymbols(rangeTimeInput, 'change');
 
+// Slider
 
+const sliders = (slides) => {
+  let slideIndex = 1;
+  const items = document.querySelectorAll(slides);
+
+
+  function showSlides(n) {
+      if (n > items.length) {
+          slideIndex = 1;
+      }
+
+      if (n < 1) {
+          slideIndex = items.length;
+      }
+
+      items.forEach(item => {
+          item.style.display = 'none';
+      });
+
+      items[slideIndex - 1].style.display = 'block';
+  }
+
+  showSlides(slideIndex);
+
+  function plusSlides(n) {
+      showSlides(slideIndex += n);
+  }
+
+
+  try {
+      let initialPoint;
+      let finalPoint;
+      items.forEach((item) => {
+          item.addEventListener('touchstart', (e) => {
+              e.preventDefault();
+              e.stopPropagation();
+              initialPoint = e.changedTouches[0];
+          }, false);
+      });
+      items.forEach(item => {
+          item.addEventListener('touchend', (e) => {
+              e.preventDefault();
+              e.stopPropagation();
+              finalPoint = e.changedTouches[0];
+              let xAbs = Math.abs(initialPoint.pageX - finalPoint.pageX);
+              let yAbs = Math.abs(initialPoint.pageY - finalPoint.pageY);
+              if (xAbs > 20 || yAbs > 20) {
+                  if (xAbs > yAbs) {
+                      if (finalPoint.pageX < initialPoint.pageX) {
+                          plusSlides(-1);
+                      } else {
+                          plusSlides(1);
+                      }
+                  }
+              }
+          }, false);
+      });
+
+  } catch (e) {}
+};
+
+sliders('.about__inner-image-mobile');
+sliders('.reviews__item-mobile');
+
+function accord(headers, activeClass, paddingBottom) {
+
+    
+    const headers_ = document.querySelectorAll(headers);
+
+    headers_.forEach(item => {
+        item.addEventListener('click', () => {
+            item.parentElement.classList.toggle(activeClass);
+            if (item.parentElement.classList.contains(activeClass)) {
+                item.nextElementSibling.style.cssText = `max-height: ${item.nextElementSibling.scrollHeight + paddingBottom}px; padding-bottom: ${paddingBottom}px;`;
+            } else {
+                item.nextElementSibling.style.cssText = `max-height: 0px; padding-bottom: 0px;`;
+            }
+        });
+    });
+}
+
+accord('.faq__item__header', 'faq__item--active', 20);
+
+});
 
